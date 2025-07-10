f51f430ccb7a60e14581df3175e22007ec104e23
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-10 11:50:26 -0600

    now parses arguments for '--verbose' and '-v' flags

--- main.py
+++ main.py
@@ -8,16 +8,37 @@ from google.genai import types
 def main():
     load_dotenv()
 
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
     args = sys.argv[1:]
 
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt goes here"')
         sys.exit(1)
-    user_prompt = " ".join(args)
 
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+    # flags = ["--verbose", "-v"]
+
+    verbose = False
+    
+    parse_flags = True
+    prompt_args = []
+    for arg in args:
+        if arg == "--":
+            parse_flags = False
+            continue
+        if parse_flags and (arg == "--verbose" or arg == "-v"):
+            verbose = True
+        else:
+            prompt_args.append(arg)
+
+    if len(prompt_args) == 0:
+        print("Please provide a prompt.")
+        print('Usage: python main.py "your prompt goes here"')
+        sys.exit(1)
+
+    user_prompt = " ".join(prompt_args)
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -27,10 +48,14 @@ def main():
         model='gemini-2.0-flash-001', contents=messages
     )
 
+    if verbose:
+        print(f'User prompt: {user_prompt}\n')
+
     print(response.text)
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
-    
+
+    if verbose:
+        print(f'\nPrompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
 
 
 if __name__ == "__main__":
