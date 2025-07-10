fd506d6dcac98ca0d3ca328090f10682cd538d14
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-10 09:38:40 -0600

    added 'messages' list variable to store conversation

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 
 def main():
     load_dotenv()
 
-    args = sys.args[1:]
+    args = sys.argv[1:]
 
     if not args:
         print("AI Code Assistant")
@@ -19,9 +19,12 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    contents = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=user_prompt
+        model='gemini-2.0-flash-001', contents=messages
     )
 
     print(response.text)
