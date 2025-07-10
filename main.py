36accf3c05286b8ffe8a5ba979c8098506e7d4dd
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-10 09:32:20 -0600

    added 'usage' comment when no args are provided

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Unable to find prompt. Please include a single prompt in quotation marks in the command following the file name.")
+    load_dotenv()
+
+    args = sys.args[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt goes here"')
         sys.exit(1)
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
     contents = sys.argv[1]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=contents
+        model='gemini-2.0-flash-001', contents=user_prompt
     )
 
     print(response.text)
