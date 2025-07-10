afd152daccc58b07848f5e24e4ad9dbd632090f0
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-10 09:00:24 -0600

    makes request with test prompt, and prints response and token count

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ client = genai.Client(api_key=api_key)
 
 def main():
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
     )
 
-    print(response)
+    print(response.text)
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    
 
 
 if __name__ == "__main__":
