19af1058e5279bdf48edea419c181612c1f1e1ca
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-13 22:57:33 -0600

    Updated tests to run within a function

--- tests.py
+++ tests.py
@@ -1,18 +1,27 @@
 from functions.get_file_info import get_files_info
 
 
-print("--- Test 1 ---")
-test_1_result = get_files_info("calculator", ".")
-print(test_1_result)
+def tests():
+    print("--- Test 1 ---")
+    test_1_result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(test_1_result)
 
-print("--- Test 2 ---")
-test_2_result = get_files_info("calculator", "pkg")
-print(test_2_result)
+    print("--- Test 2 ---")
+    test_2_result = get_files_info("calculator", "pkg")
+    print('Result for "pkg" directory:')
+    print(test_2_result)
 
-print("--- Test 3 ---")
-test_3_result = get_files_info("calculator", "/bin")
-print(test_3_result)
+    print("--- Test 3 ---")
+    test_3_result = get_files_info("calculator", "/bin")
+    print('Result for "/bin" directory:')
+    print(test_3_result)
 
-print("--- Test 4 ---")
-test_4_result = get_files_info("calculator", "../")
-print(test_4_result)
+    print("--- Test 4 ---")
+    test_4_result = get_files_info("calculator", "../")
+    print('Result for "../" directory:')
+    print(test_4_result)
+
+
+if __name__ == "__main__":
+    tests()
