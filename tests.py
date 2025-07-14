c3fc6280274ade84d7382de7cd7cfe958bd5ffa2
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-07-14 11:37:00 -0600

    added tests for 'get_file_content()' function

--- tests.py
+++ tests.py
@@ -1,26 +1,58 @@
-from functions.get_file_info import get_files_info
+# from functions.get_file_info import get_files_info
+from functions.get_file_content import get_file_content
+# from config import MAX_CHARS
 
 
 def tests():
-    print("--- Test 1 ---")
-    test_1_result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(test_1_result)
+    # --- Tests for 'get_file_content' ---
+    # main.py
+    print("Result for 'main.py':")
+    result = get_file_content("calculator", "main.py")
+    print(result)
 
-    print("--- Test 2 ---")
-    test_2_result = get_files_info("calculator", "pkg")
-    print('Result for "pkg" directory:')
-    print(test_2_result)
+    # pkg/calculator.py
+    print("Result for 'pkg/calculator.py':")
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
 
-    print("--- Test 3 ---")
-    test_3_result = get_files_info("calculator", "/bin")
-    print('Result for "/bin" directory:')
-    print(test_3_result)
+    # /bin/cat
+    print("Result for '/bin/cat':")
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
 
-    print("--- Test 4 ---")
-    test_4_result = get_files_info("calculator", "../")
-    print('Result for "../" directory:')
-    print(test_4_result)
+    # Lorem.txt
+    # print("Result from 'get_file_content()' for 'lorem.txt':")
+    # working_directory = "calculator"
+    # file_path = "lorem.txt"
+    # result = get_file_content(working_directory, file_path)
+    #
+    # # result = get_file_content("calculator", "lorem.txt")
+    # is_truncated = result.endswith(
+    #     f'...File "{file_path}" truncated at {MAX_CHARS} characters]'
+    # )
+    # print(f"'lorem.txt' is truncated: {is_truncated}")
+    # print(result)
+
+    # --- Tests for 'get_file_info' ---
+    # print("--- Test 1 ---")
+    # test_1_result = get_files_info("calculator", ".")
+    # print("Result for current directory:")
+    # print(test_1_result)
+    #
+    # print("--- Test 2 ---")
+    # test_2_result = get_files_info("calculator", "pkg")
+    # print('Result for "pkg" directory:')
+    # print(test_2_result)
+    #
+    # print("--- Test 3 ---")
+    # test_3_result = get_files_info("calculator", "/bin")
+    # print('Result for "/bin" directory:')
+    # print(test_3_result)
+    #
+    # print("--- Test 4 ---")
+    # test_4_result = get_files_info("calculator", "../")
+    # print('Result for "../" directory:')
+    # print(test_4_result)
 
 
 if __name__ == "__main__":
